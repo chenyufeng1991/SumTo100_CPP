8c195a083b740ae42062cd63561d32e5799e03f2
陈宇峰 <yufengcode@gmail.com>
2016-09-25 17:38:37 +0800

    init

--- SumTo100_C++/main.cpp
+++ SumTo100_C++/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {44,66,1,2,0,99,55,45};
+    vector<int> vectorArray(arr, arr + sizeof(arr)/sizeof(int));
+
+    // 先进行排序
+    sort(vectorArray.begin(), vectorArray.end());
+
+    long i,j;
+    bool isFound = false;
+
+    for (i = 0,j = vectorArray.size() - 1; i < vectorArray.size() && i < j;)
+    {
+        if (vectorArray[i] + vectorArray[j] == 100)
+        {
+            isFound = true;
+            cout << vectorArray[i] << "   " << vectorArray[j] << endl;
+            // 可能会有多对，所以要继续查找
+            i++;
+            j--;
+        }
+        else if (vectorArray[i] + vectorArray[j] < 100)
+        {
+            i++;
+        }
+        else if (vectorArray[i] + vectorArray[j] > 100)
+        {
+            j--;
+        }
+    }
+
+    if (!isFound)
+    {
+        cout << "not found" << endl;
+    }
+
     return 0;
 }
